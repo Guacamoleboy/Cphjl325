d3e8781b781efe58c2ecb915077fe927edc875b3
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-02 20:29:51 +0100

    Monday

--- Mandags opgaver/5. Mandag/Task_1/MyDrinks.java
+++ Mandags opgaver/5. Mandag/Task_1/MyDrinks.java
@@ -1,9 +1,30 @@
+import java.util.Arrays; // Allows us to use Arrays.toString
+import java.util.ArrayList; // Allows us to use Arraylist
+
 public class MyDrinks
 {
+
     public static void main(String[] args)
     {
 
-        System.out.println("List of Drinks..");
+        ArrayList <String> drinks = new ArrayList <String> (); // Without <String> in new ArrayList it gives "unsafe" error.
+
+        drinks.add("Pepsi Max");
+        drinks.add("Coca Cola");
+        drinks.add("Dr. Pepper");
+        drinks.add("Faxe Kondi");
+        drinks.add("Faxe Kondi Booster");
+        drinks.add("Monster Energy");
+
+        for(String s : drinks){
+            
+            System.out.println(s);
+
+        }
         
-    }
-}
+        
+       
+        
+    } // Main Method End
+
+} // Class End
