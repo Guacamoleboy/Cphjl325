95d1cc6676ff3fc7b07f938c0a5b27ae4b577b4d
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-03 08:51:22 +0100

    .

---
+++ Random/java-prep/test.java
@@ -0,0 +1,10 @@
+class Test // Class
+{
+    public static void main(String[] args) // Main method. REQUIRED IN JAVA!
+    {
+        
+        System.out.println("My First Java Program.\n Testing.");
+        System.out.println("\nThis is a random line.");
+
+    } // Main method end
+} // Class end
