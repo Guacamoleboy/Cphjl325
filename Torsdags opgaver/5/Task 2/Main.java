f19df9c56d4ecbf04f9069f6d689c6ec319eccf2
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-05 14:52:13 +0100

    Yuh

--- Torsdags opgaver/5/Task 2/Main.java
+++ Torsdags opgaver/5/Task 2/Main.java
@@ -9,12 +9,26 @@
 
 */
 
+import java.util.Scanner;
+
 public class Main{
 	
 	public static void main(String[] args){
 
+	int ageTillRetirement = 67;
 
+	Scanner scanner = new Scanner(System.in);
 
+	System.out.println("Please type your name:");
+	String name = scanner.nextLine();
+
+	System.out.println("Please type your age:");
+	int age = scanner.nextInt();
+
+	int ageLeft = ageTillRetirement - age;
+
+	// Output
+	System.out.println("Hello, " + name + ". You are " + age + " years old.. You have " + ageLeft + " left untill retirement.");
 
 	}
 
