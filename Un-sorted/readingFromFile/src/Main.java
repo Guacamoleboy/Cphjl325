216a843d8014b65707aa410ab38a5a31f9b395c2
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-11 11:53:31 +0100

    Update Main.java

--- Un-sorted/readingFromFile/src/Main.java
+++ Un-sorted/readingFromFile/src/Main.java
@@ -6,35 +6,7 @@ public class Main { // Client class
 
     public static void main(String[] args) { // Main class
 
-        File file = new File("data/teams.csv"); // data/ is just our folder
 
-        try {
-
-            Scanner scan = new Scanner(file);
-            scan.nextLine(); // Skips header
-
-            while(scan.hasNextLine()){
-
-                // Splits our .cvs into an array
-                String line = scan.nextLine();
-                String[] values = line.split(",");
-
-                // Stores our values
-                String groupName = values[0];
-                int groupID = Integer.parseInt(values[1].trim()); // trim() is to remove the spacing "   " else it'll fail
-                int groupScore = Integer.parseInt(values[2].trim());
-
-                // Creates and prints our values
-                Team t = new Team(groupName, groupID, groupScore);
-                System.out.println(t); // Can use t.toString() too.
-
-            } // while end
-
-        } catch (FileNotFoundException e) { // Always try to catch the specific exception!
-
-            System.out.println("File not found..");
-
-        } // Try-catch end
 
     } // Main class end
 
@@ -73,5 +45,40 @@ public class Main { // Client class
 
     } // readPresidents method end
 
+    // ___________________________________________________________________________
+
+    public static void teamsMethod(){
+
+        File file = new File("data/teams.csv"); // data/ is just our folder
+
+        try {
+
+            Scanner scan = new Scanner(file);
+            scan.nextLine(); // Skips header
+
+            while(scan.hasNextLine()){
+
+                // Splits our .cvs into an array
+                String line = scan.nextLine();
+                String[] values = line.split(",");
+
+                // Stores our values
+                String groupName = values[0];
+                int groupID = Integer.parseInt(values[1].trim()); // trim() is to remove the spacing "   " else it'll fail
+                int groupScore = Integer.parseInt(values[2].trim());
+
+                // Creates and prints our values
+                Team t = new Team(groupName, groupID, groupScore);
+                System.out.println(t); // Can use t.toString() too.
+
+            } // while end
+
+        } catch (FileNotFoundException e) { // Always try to catch the specific exception!
+
+            System.out.println("File not found..");
+
+        } // Try-catch end
+
+    } // teamsMethod end
 
 } // Client class end
